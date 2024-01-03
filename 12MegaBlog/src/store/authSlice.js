f9c0dd077109f188380379ffae5937a8c347a2c1
Jay <jaykhurana16777@gmail.com>
2024-01-03 23:24:15 +0530

    debug

--- 12MegaBlog/src/store/authSlice.js
+++ 12MegaBlog/src/store/authSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  status: false,
-  userData: null
+    status : false,
+    userData: null
 }
 
 const authSlice = createSlice({
@@ -10,16 +10,16 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         login: (state, action) => {
-            state.status= true;
-            state.userData= action.payload.userData;
+            state.status = true;
+            state.userData = action.payload.userData;
         },
         logout: (state) => {
-            state.status= false;
+            state.status = false;
             state.userData = null;
         }
-    }
+     }
 })
 
-export const {login,logout} = authSlice.actions;
+export const {login, logout} = authSlice.actions;
 
 export default authSlice.reducer;
